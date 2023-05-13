0940de56a9d3fa36033815f892482c02c92ae955
Scott <saseaa37@gmail.com>
2023-05-12 21:32:24 -0600

    S4 @ 1:16

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
 
 def index(request):
     return render(request, "home.html")
 
 def signup(request):
     form = UserCreationForm()
-
-
+    return render(request, "auth/signup.html", {'form':form})
